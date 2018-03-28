373249d2302170f30d3cc4d3b9ea40368d3521e5
Felipe Lamim <fclamim@gmail.com>
2018-03-27 21:13:45 -0300

    Anotações

--- src/control/Calculadora.java
+++ src/control/Calculadora.java
@@ -1,18 +1,23 @@
 package control;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public class Calculadora {
+	@WebMethod
 	public float Somar(float a, float b) {
 		return a + b;
 	}
+	@WebMethod
 	public float Multiplicar(float a, float b) {
 		return a*b;
 	}
+	@WebMethod
 	public float Dividir(float a, float b) {
 		return a/b;
 	}
+	@WebMethod
 	public float Substrair(float a, float b) {
 		return a - b;
 	}
